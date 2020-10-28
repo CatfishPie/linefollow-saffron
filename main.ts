6ebd2cb94a33060ae05c37c7dfb7ba603ee9c819
CatfishPie <73447827+CatfishPie@users.noreply.github.com>
2020-10-28 16:44:22 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
-function LineFollowRight (inches: number) {
+function LineFollowRight (inches: number, speed: number) {
     motors.largeAD.reset()
-    LineFollow_DegreesTurned = 0
     while (motors.largeA.angle() / 360 <= inches / 5.35) {
-        motors.largeAD.steer((sensors.color3.light(LightIntensityMode.Reflected) - 50) * 0.8, 35)
+        motors.largeAD.steer((sensors.color3.light(LightIntensityMode.Reflected) - 50) * 0.8, speed)
     }
 }
-let LineFollow_DegreesTurned = 0
-LineFollowRight(10)
+LineFollowRight(10, 35)
